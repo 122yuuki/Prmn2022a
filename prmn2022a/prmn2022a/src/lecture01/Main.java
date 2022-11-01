41fdd2b3a6238f7fa92ac7bf85c81e3136880be4
yuki nishida <b2211830@photon.chitose.ac.jp>
2022-11-01 18:28:27 +0900

    課題1-2

--- prmn2022a/prmn2022a/src/lecture01/Main.java
+++ prmn2022a/prmn2022a/src/lecture01/Main.java
@@ -3,5 +3,8 @@ package lecture01;
 public class Main {
     public static void main(String[] args) {
         System.out.println("B2211830 Yuki Nishida");
+
+        int studentNumber = 2211830;
+        System.out.println("B" + studentNumber + " Yuki Nishida");
     }
 }
