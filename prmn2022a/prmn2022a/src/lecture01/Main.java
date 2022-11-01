6daf201af64f03f44c8258b454998ff7da1ccaff
yuki nishida <b2211830@photon.chitose.ac.jp>
2022-11-01 18:49:46 +0900

    課題1-3

--- prmn2022a/prmn2022a/src/lecture01/Main.java
+++ prmn2022a/prmn2022a/src/lecture01/Main.java
@@ -1,4 +1,5 @@
 package lecture01;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,5 +7,17 @@ public class Main {
 
         int studentNumber = 2211830;
         System.out.println("B" + studentNumber + " Yuki Nishida");
+
+        Scanner inputAge = new Scanner(System.in);
+        System.out.print("age = ");
+        int age = inputAge.nextInt();
+
+        if(age<20){
+            System.out.println("I am " + age + " years old so I cannot drink lipuor.");
+        }
+        else{
+            System.out.println("I am " + age + " years old so I can drink lipuor.");
+        }
+
     }
 }
