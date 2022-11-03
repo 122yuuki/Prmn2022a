a3a4d537543b4ee5deaa2585c914419b497db522
yuki nishida <b2211830@photon.chitose.ac.jp>
2022-11-04 01:31:36 +0900

    実験１

--- prmn2022a/prmn2022a/src/lecture02/Exercise2_3.java
+++ prmn2022a/prmn2022a/src/lecture02/Exercise2_3.java
@@ -1,17 +1,17 @@
-//package lecture02;
-//
-//public class Exercise2_3{
-//    public static void main(String[] args) {
-//
-//            Tire[] tires = new Tire[4];
-//            Engine engine = new Engine(4000);
-//            Car2 car = new Car2(tires,engine);
-//            GasStation2 gasStation = new GasStation2();
-//
-//            gasStation.refuel(car);
-//            car.startEngine();
-//            car.run();
-//
-//
-//    }
+package lecture02;
+
+public class Exercise2_3{
+    public static void main(String[] args) {
+
+            Tire[] tires = new Tire[4];
+            Engine engine = new Engine(4000);
+            Car2 car = new Car2(tires,engine);
+            GasStation2 gasStation = new GasStation2();
+
+            gasStation.refuel(car);
+            car.startEngine();
+            car.run();
+
+
+    }
 }
