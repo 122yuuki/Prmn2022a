3d043150ab689feb71fa7cacc767cec52ab51037
yuki nishida <b2211830@photon.chitose.ac.jp>
2022-11-03 22:41:55 +0900

    課題1

---
+++ prmn2022a/prmn2022a/src/lecuture02/Exercise2_1.java
@@ -0,0 +1,16 @@
+package lecuture02;
+
+public class Exercise2_1 {
+    public static void main(String[] args) {
+
+        Human human1 = new Human("ゆうき",18);
+        human1.print();
+
+        Human human2 = new Human("たいち",20);
+        human2.print();
+
+        Human human3 = new Human("ごりら",50);
+        human3.print();
+
+    }
+}
