ff7ce823af665b5a4461e0166b3639ebb189e441
yuki nishida <b2211830@photon.chitose.ac.jp>
2022-11-06 10:56:28 +0900

    課題１

---
+++ prmn2022a/prmn2022a/src/lecture03/Exercise3_1.java
@@ -0,0 +1,14 @@
+package lecture03;
+
+import java.util.Scanner;
+
+public class Exercise3_1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("任意の文字列を入力してください：");
+        String m = input.next();
+        System.out.print("”" + m + "” と入力されました");
+
+    }
+}
