43ea117e0d86241855d691274d85b1b0021ac381
yuki nishida <b2211830@photon.chitose.ac.jp>
2022-11-09 14:31:20 +0900

    課題4-1

---
+++ prmn2022a/prmn2022a/src/lecture04/Exercise4_1.java
@@ -0,0 +1,35 @@
+package lecture04;
+
+public class Exercise4_1 {
+    public static void main(String[] args) {
+
+        Fighter my = new Fighter(100,40,"hogehoge");
+        Fighter you = new Fighter(300,10,"maou");
+
+        System.out.println(my.getName() + "のHP：" + my.getHitPoint());
+        System.out.println(you.getName() + "のHP：" + you.getHitPoint());
+        System.out.println();
+
+        while(true){
+
+            my.attack(you);   //
+            System.out.println(you.getName() + " の残り　hitpoint ：" + you.getHitPoint());
+            if(you.isAlive()){
+                System.out.print(you.getName() + " は倒れた。");
+                break;
+            }
+            you.attack(my);
+            System.out.println(my.getName() + " の残り　hitpoint ：" + my.getHitPoint());
+            if(my.isAlive()){
+                System.out.println(my.getName() + " は倒れた。");
+                break;
+            }
+            System.out.println();
+
+        }
+
+    }
+}
+
+
+
