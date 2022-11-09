efcd95c61557539c4184f2c598ef2c18ebdfb6ec
yuki nishida <b2211830@photon.chitose.ac.jp>
2022-11-09 11:29:00 +0900

    課題3-4

--- prmn2022a/prmn2022a/src/lecture03/Exercise3_4.java
+++ prmn2022a/prmn2022a/src/lecture03/Exercise3_4.java
@@ -1,17 +1,14 @@
 package lecture03;
 
-import java.sql.Array;
+import java.util.Scanner;
 import java.util.ArrayList;
 public class Exercise3_4 {
     public static void main(String[] args) {
 
-        ArrayList<Vegetable> v = new ArrayList<>();
-        Vegetable v1 = new Vegetable("にんじん",117);
-        Vegetable v2 = new Vegetable("たまねぎ",120);
-        Vegetable v3 = new Vegetable("にんじん",154);
-        v.add(v1);
-        v.add(v2);
-        v.add(v3);
+        ArrayList<Vegetable> v = new ArrayList<Vegetable>();
+        v.add(new Vegetable("にんじん",117));
+        v.add(new Vegetable("たまねぎ",120));
+        v.add(new Vegetable("じゃがいも",154));
 
         for(int i=0;i<v.size();i++){
             v.get(i).print();
